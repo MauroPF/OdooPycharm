16351c4233c76a318869e4238994a7527ff6c813
mauro <mperezfandino@danielcastelao.org>
2021-03-23 18:37:00 +0100

    Añadido acciones y menús

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -24,6 +24,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
